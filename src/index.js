691fad4e487e2b970358b0e1f435457e20aa44f4
潘世文 <panshiwen@panshiwendeMacBook-Pro.local>
2020-11-16 15:07:40 +0800

    提交history

--- src/index.js
+++ src/index.js
@@ -7,21 +7,40 @@ import React, {
 import ReactDOM from "react-dom";
 import "./index.css";
 
-function App(props) {
-  let [number, setNumber] = useState(0);
-  const inputRef = useRef(null);
-  console.log("App -> inputRef", inputRef);
-
-  const getFocus = () => {
-    inputRef.current.focus();
-  };
-
+//一定是第个参数
+function Child(props, parentRef) {
+  let inputRef = useRef();
+  useImperativeHandle(parentRef, () => ({
+    focus() {
+      inputRef.current.focus();
+    },
+    setValue(newVal) {
+      inputRef.current.value = newVal;
+    },
+  }));
   return (
-    <div>
-      <button onClick={getFocus}>获取焦点</button>
-      <button onClick={() => setNumber(number + 1)}>+</button>
-    </div>
+    <>
+      <input type="text" ref={inputRef} />
+    </>
+  );
+}
+let ForwardedChild = forwardRef(Child);
+function Parent() {
+  let [number, setNumber] = useState(0);
+  let parentRef = useRef(); //{current:null} //每次都会返回同一个对象
+  function getFocus() {
+    //希望在父组件只能调用focus方法，其它的方法都不能调用
+    parentRef.current.focus(); //current是此input框 的真实DOM元素
+    //inputRef.current.value = 'something';
+    parentRef.current.setValue("something");
+  }
+  return (
+    <>
+      <ForwardedChild ref={parentRef} />
+      <button onClick={() => setNumber((prevState) => prevState + 1)}>+</button>
+      <button onClick={getFocus}>获得焦点</button>
+    </>
   );
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Parent />, document.getElementById("root"));
